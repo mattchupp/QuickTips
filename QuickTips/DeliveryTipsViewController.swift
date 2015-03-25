e7f38a14513c90fae91c9fd89ff2760526fcf7ab
Matthew Chupp <mattlchupp@gmail.com>
2015-03-25 18:40:24 -0500

    wired up buttons for delivery tips

--- QuickTips/DeliveryTipsViewController.swift
+++ QuickTips/DeliveryTipsViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class DeliveryTipsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var tipPercentage: UISegmentedControl!
+    
+    
     // load the view from the xib
     override func loadView() {
         NSBundle.mainBundle().loadNibNamed("DeliveryTipsViewController", owner: self, options: nil)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        billAmount.borderStyle = UITextBorderStyle.None
+        
+        billAmount.becomeFirstResponder()
+        tipAmount.text = "$0.00"
+        totalAmount.text = "$0.00"
+        
+        
+    }
+    
+    
+    @IBAction func tipAmountSelected(sender: UISegmentedControl) {
+        
+        var tipValues = [0.18, 0.20, 0.22]
+        var billString = billAmount.text
+        var billDouble = Double((billString as NSString).doubleValue)
+        var tip = billDouble * tipValues[tipPercentage.selectedSegmentIndex]
+        var total = billDouble + tip
+        
+        tipAmount.text = String(format: "$%.2f", tip)
+        totalAmount.text = String(format: "$%.2f", total)
+        
+    }
+
 
 }
